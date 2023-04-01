4017a56491cd696c1d1016a67308e65f616d0aa4
mami-em <maminiainaravaloarison@gmail.com>
2023-04-01 14:23:29 -0400

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace ToDo
+{
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Check for valid card
+        static bool IsValid(string cardNumber)
+        {
+            int sum = 0;
+            bool isSecondDigit = false;
+            for (int i = cardNumber.Length - 1; i >= 0; i--)
+            {
+                if (cardNumber[i] < '0' || cardNumber[i] > '9')
+                {
+                    // Invalid character, return false
+                    return false;
+                }
+                int digit = cardNumber[i] - '0';
+                if (isSecondDigit)
+                {
+                    digit *= 2;
+                    if (digit > 9)
+                    {
+                        digit -= 9;
+                    }
+                }
+                sum += digit;
+                isSecondDigit = !isSecondDigit;
+            }
+            return sum % 10 == 0;
+        }
+
+        // Get the card type
+        static string GetCardType(string cardNumber)
+        {
+            if (cardNumber.Length == 15 && (cardNumber.StartsWith("34") || cardNumber.StartsWith("37")))
+            {
+                return "Amex";
+            }
+            else if (cardNumber.Length == 16 && (cardNumber.StartsWith("51") || cardNumber.StartsWith("52") || cardNumber.StartsWith("53") || cardNumber.StartsWith("54") || cardNumber.StartsWith("55")))
+            {
+                return "MasterCard";
+            }
+            else if ((cardNumber.Length == 13 || cardNumber.Length == 16) && cardNumber.StartsWith("4"))
+            {
+                return "Visa";
+            }
+            else
+            {
+                return "Unknown";
+            }
+        }
+
+        // Our button onclick
+        private async void OnSubmitButtonClicked(object sender, EventArgs e)
+        { 
+
+            string cardNumber = entryCardNumber.Text;
+
+            if (cardNumber.Length == 0)
+            {
+                await DisplayAlert("Alert", "Please Enter a card number", "OK");
+                return;
+            }
+
+            if (IsValid(cardNumber))
+            {
+                // await DisplayAlert("Card Type", $"{GetCardType(cardNumber)}", "OK");
+
+                // Create a new label with the value of the entry
+                Label newLabel = new Label
+                {
+                    Text = cardNumber + " - " + GetCardType(cardNumber),
+                    FontSize = 18,
+                };
+
+                Frame frame = new Frame
+                {
+                    Content = newLabel,
+                    BorderColor = Colors.LightGray,
+                    CornerRadius = 10,
+                };
+
+
+                // Add the new label to the stack layout
+                stackLayout.Children.Add(frame);
+            }
+
+            else
+                await DisplayAlert("Error", "Invalid Card", "OK");
+
+            entryCardNumber.Text = "";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,72 @@
+# Card Validator
+This is a simple `.NET MAUI` application that checks if a credit card number is valid and identifies its type (Visa, MasterCard, Amex or Unknown), based on a project from [Harvard CS50x](https://cs50.harvard.edu/x/2023/psets/1/credit/).
+
+## Requirements
+* `XCODE`
+* Visual Studio 
+* `.NET MAUI`
+* Microsoft.Maui.Controls
+
+## Screenshots
+<!-- ![Desktop Format](screenshots/desktop.png)
+![IOS Format](screenshots/IOS.png)
+![Success](screenshots/Success.png)
+![Empty input](screenshots/empty_input.png)
+![Invalid Card](screenshots/invalid_card.png) -->
+<table>
+    <tr>
+        <td>
+            <a href="">
+                <img src="screenshots/desktop.png" alt="Screenshot Home desktop" width="300px" />
+            </a>
+        </td>
+        <td>
+            <a href="">
+                <img src="screenshots/Ipad.png" alt="Screenshot home IOS" width="300px" />
+            </a>
+        </td>
+        <td>
+            <a href="">
+                <img src="screenshots/Iphone.png" alt="Screenshot home Iphone" width="300px" />
+            </a>
+        </td>
+        <td>
+            <a href="">
+                <img src="screenshots/Android.png" alt="Screenshot home Android" width="300px" />
+            </a>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <a href="">
+                <img src="screenshots/empty_input.png" alt="Screenshot Empty input" width="300px" />
+            </a>
+        </td>
+        <td>
+            <a href="">
+                <img src="screenshots/invalid_card.png" alt="Screenshot Invalid Card" width="300px" />
+            </a>
+        </td>
+        <td>
+            <a href="">
+                <img src="screenshots/insert_data.png" alt="Screenshot inserting card" width="300px" />
+            </a>
+        </td>
+        <td>
+            <a href="">
+                <img src="screenshots/success_input.png" alt="Screenshot successful card" width="300px" />
+            </a>
+        </td>
+        <td>
+            <a href="">
+                <img src="screenshots/success.png" alt="Screenshot all type of card" width="300px" />
+            </a>
+        </td>
+    </tr>
+</table>
+
+## Usage
+To use this application, simply run it on your local machine or emulator, and input a credit card number into the text box provided. If the card number is valid, the application will add a label showing the card number and its type (Visa, MasterCard, Amex or Unknown) to the stack layout. If the card number is invalid, the application will display an error message.
+
+## How it works
+This application checks if the entered credit card number is valid using the Luhn algorithm. It then identifies the type of the credit card based on the first few digits of the card number. If the card number is valid, a new label is created with the card number and its type, and added to the stack layout.
